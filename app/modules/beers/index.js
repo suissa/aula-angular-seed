1949fc699bbef6957da67ad752f64f4a1d887680
Suissa <jnascimento@gmail.com>
2015-07-30 00:02:43 -0300

    Subindo create view

--- app/modules/beers/index.js
+++ app/modules/beers/index.js
@@ -2,7 +2,8 @@
 
 angular.module('myApp.Beers', ['ngRoute'])
 .config(['$routeProvider', beersConfig])
-.controller('BeerListController', BeerListController);
+.controller('BeerListController', BeerListController)
+.controller('BeerCreateController', BeerCreateController);
 
 // Config
 function beersConfig($routeProvider){
@@ -14,7 +15,6 @@ function beersConfig($routeProvider){
 beersConfig['$inject'] = ['$routeProvider'];
 
 // Controllers
-
 function BeerListController($scope, $http) {
   var httpRequest = {
         url: 'http://localhost:3000/api/beers'
@@ -36,7 +36,29 @@ function BeerListController($scope, $http) {
 
 };
 
+function BeerCreateController($scope, $http) {
+  var httpRequest = {
+        url: 'http://localhost:3000/api/beers'
+      , method: 'POST'
+      }
+    ;
+
+  $http(httpRequest)
+  .success(function(data) {
+    console.log('SUCESSO: ', data);
+    // $scope.beers = data;
+    $scope.msg = 'Cadastro da cerveja feito com sucesso.';
+  })
+  .error(function(err) {
+    console.log('ERRO: ', err);
+    $scope.msg = 'Cadastro da cerveja não pode ser feito.';
+
+  });
+
+};
+
 BeerListController.$inject = ['$scope', '$http'];
+BeerCreateController.$inject = ['$scope', '$http'];
 
 
 
